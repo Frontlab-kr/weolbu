3b755a96f72038abce62c8a1b5a10677bcf7917d
Frontlab-kr <contact@frontlab.kr>
2023-03-21 11:06:37 +0900

    first commit

---
+++ doc/confirmpopup/ConfirmationServiceDoc.vue
@@ -0,0 +1,23 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>ConfirmPopup is controlled via the <i>ConfirmationService</i> that needs to be installed as an application plugin.</p>
+    </DocSectionText>
+    <DocSectionCode :code="code" hideToggleCode importCode hideCodeSandbox hideStackBlitz />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            code: {
+                basic: `
+import {createApp} from 'vue';
+import ConfirmationService from 'primevue/confirmationservice';
+
+const app = createApp(App);
+app.use(ConfirmationService);`
+            }
+        };
+    }
+};
+</script>
---
+++ doc/toast/ToastServiceDoc.vue
@@ -0,0 +1,23 @@
+<template>
+    <DocSectionText v-bind="$attrs">
+        <p>Toast component is controlled via the <i>ToastService</i> that needs to be installed as an application plugin.</p>
+    </DocSectionText>
+    <DocSectionCode :code="code" hideToggleCode importCode hideCodeSandbox hideStackBlitz />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            code: {
+                basic: `
+import {createApp} from 'vue';
+import ToastService from 'primevue/toastservice';
+
+const app = createApp(App);
+app.use(ToastService);`
+            }
+        };
+    }
+};
+</script>
